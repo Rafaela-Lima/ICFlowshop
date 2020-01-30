2bcc157e7003b32312c762aee1065633874a1792
Rafaela-Lima <Rafaela-Lima>
2020-01-29 21:57:02 -0300

    refatoração pausada

--- gerandoperm.c
+++ gerandoperm.c
@@ -2,52 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-//calculo do makespan no calculo;
-//uma permutação pode ser igual a outra;
 
 int main(int argc, char const *argv[])
 {
-    int pop[10][4], i, j, k;
-    bool foi = false;
-    srand(time(NULL));
+    int L, C;
+    int flag;
+    scanf("%d%d", &L, &C);
 
-    for (i = 0; i < 10; i++) //linhas;
+    int populacao[L][C];
+
+    for (int i = 0; i < L; i++)
     {
-        do // verificar se linhas são diferentes;
+        for (int j = 0; j < C; j++)
         {
-            foi = false;
-            for (j = 0; j < 4; j++) //colunas;
+            populacao[i][j] = -1;
+        }
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < L; i++)
+    {
+        while (1)
+        {
+            int randon = rand() % C;
+            for (int j = 0; j < C; j++)
             {
-                do //colunas diferentes;
+                if (randon == populacao[i][j])
                 {
-                    foi = false;
-                    pop[i][j] = rand() % 4 + 1;
-                    for (k = 0; k < j; k++)
-                    {
-                        if (pop[i][j] == pop[i][k])
-                        {
-                            foi = true;
-                            break;
-                        }
-                    }
-                } while (foi);
-            }
-            for (k = 0; k < i; k++)
-            {
-                if ((pop[i][0] == pop[k][0]) && (pop[i][1] == pop[k][1]) && (pop[i][2] == pop[k][2]) && (pop[i][3] == pop[k][3])) //deixar generico para N jobs;
-                {
-                    foi = true;
+                    flag = 0;
                     break;
                 }
+                else
+                {
+                    flasg = 1;
+                }
             }
-        } while (foi);
+        }
+        populacao[i][j] = randon;
     }
-    for (i = 0; i < 10; i++)
+    for (int i = 0; i < L; i++)
     {
-        for (j = 0; j < 4; j++)
+        for (int j = 0; j < C; j++)
         {
-            printf("%d", pop[i][j]);
+            printf("%d\t", populacao[i][j]);
         }
         printf("\n");
     }
+
+    return 0;
 }
